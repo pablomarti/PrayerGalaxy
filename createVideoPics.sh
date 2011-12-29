25ef004aa46e14aad2a7bb0a44108548d50252b1
Pablo Marti <pablo@martigomez.martigomez.com>
2011-12-29 15:38:37 -0600

    cambios en video controller

--- createVideoPics.sh
+++ createVideoPics.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chmod 0777 "public/worshipvideos/$1"
-#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_1.jpg"
-#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:40 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_2.jpg"
-#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:01:00 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_3.jpg"
+ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_1.jpg"
+ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:40 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_2.jpg"
+ffmpeg -i "public/worshipvideos/$1" -an -ss 00:01:00 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_3.jpg"
 echo "End of making thumbs"
