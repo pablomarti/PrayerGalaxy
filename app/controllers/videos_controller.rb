1cee7a0e12f6466a4368a050c8643d26ad89360c
Pablo Marti <pablo@martigomez.martigomez.com>
2011-12-28 22:33:19 -0600

    cambios en manejo de videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -60,6 +60,9 @@ class VideosController < AdminManagementController
         if !params[:video][:video].nil?
           fork do
             exec("./createVideoPics.sh '#{videoName}' &")
+            exec("ffmpeg -i 'public/worshipvideos/#{videoName}' -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y 'app/assets/images/worship_pics/#{videoName}_1.jpg'")
+            exec("ffmpeg -i 'public/worshipvideos/#{videoName}' -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y 'app/assets/images/worship_pics/#{videoName}_2.jpg'")
+            exec("ffmpeg -i 'public/worshipvideos/#{videoName}' -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y 'app/assets/images/worship_pics/#{videoName}_3.jpg'")
           end
 
           @video.update_attribute("pic1", "worship_pics/#{videoName}_1.jpg")
--- createVideoPics.sh
+++ createVideoPics.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_1.jpg"
-ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:40 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_2.jpg"
-ffmpeg -i "public/worshipvideos/$1" -an -ss 00:01:00 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_3.jpg"
+chmod 0777 "public/worshipvideos/$1"
+#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:20 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_1.jpg"
+#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:00:40 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_2.jpg"
+#ffmpeg -i "public/worshipvideos/$1" -an -ss 00:01:00 -r 1 -vframes 1 -f mjpeg -y "app/assets/images/worship_pics/${1}_3.jpg"
+echo "End of making thumbs"
